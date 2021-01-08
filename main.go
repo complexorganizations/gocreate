bd56090c1e8f0d4925d339aea1fb3da72681aedb
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-01-08 04:46:18 -0500

    Update main.go

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"io/ioutil"
 )
 
+// https://github.com/golang-standards/project-layout
+
 func createDirectory() {
 	os.Mkdir("api", 0755)
 	os.Mkdir("assets", 0755)
@@ -35,7 +37,7 @@ func createFiles() {
 *.so
 *.dylib
 
-# Test binary, built with `go test -c`
+# Test binary, built with 'go test -c'
 *.test
 
 # Output of the go coverage tool, specifically when used with LiteIDE
@@ -43,13 +45,27 @@ func createFiles() {
 
 # Dependency directories (remove the comment below to include it)
 # vendor/`
+
 	ioutil.WriteFile("./gitignore", []byte(gitignore), 0755)
 	ioutil.WriteFile("./api/README.md", []byte("# `/api`"), 0755)
 	ioutil.WriteFile("./assets/README.md", []byte("# `/assets`"), 0755)
 	ioutil.WriteFile("./build/README.md", []byte("# `/build`"), 0755)
 	ioutil.WriteFile("./cmd/README.md", []byte("# `/cmd`"), 0755)
-	ioutil.WriteFile("./build/README.md", []byte("# `/build`"), 0755)
-
+	ioutil.WriteFile("./config/README.md", []byte("# `/config`"), 0755)
+	ioutil.WriteFile("./deployments/README.md", []byte("# `/deployments`"), 0755)
+	ioutil.WriteFile("./docs/README.md", []byte("# `/docs`"), 0755)
+	ioutil.WriteFile("./examples/README.md", []byte("# `/examples`"), 0755)
+	ioutil.WriteFile("./githooks/README.md", []byte("# `/githooks`"), 0755)
+	ioutil.WriteFile("./init/README.md", []byte("# `/init`"), 0755)
+	ioutil.WriteFile("./internal/README.md", []byte("# `/internal`"), 0755)
+	ioutil.WriteFile("./pkg/README.md", []byte("# `/pkg`"), 0755)
+	ioutil.WriteFile("./scripts/README.md", []byte("# `/scripts`"), 0755)
+	ioutil.WriteFile("./test/README.md", []byte("# `/test`"), 0755)
+	ioutil.WriteFile("./third_party/README.md", []byte("# `/third_party`"), 0755)
+	ioutil.WriteFile("./tools/README.md", []byte("# `/tools`"), 0755)
+	ioutil.WriteFile("./vendor/README.md", []byte("# `/vendor`"), 0755)
+	ioutil.WriteFile("./web/README.md", []byte("# `/web`"), 0755)
+	ioutil.WriteFile("./website/README.md", []byte("# `/website`"), 0755)
 }
 
 func main() {
