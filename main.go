e8ab21218f67265f9b4c8de55f23698da3131c9e
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-01-08 05:07:09 -0500

    Update main.go

--- main.go
+++ main.go
@@ -8,68 +8,64 @@ import (
 // https://github.com/golang-standards/project-layout
 // https://gobyexample.com/command-line-arguments
 
-func createDirectory() {
+func createProject() {
+	// take user argument
+	argument := os.Args[1:]
+	// start creating project
+	os.Mkdir(argument, 0755)
 	os.Mkdir("api", 0755)
+	ioutil.WriteFile("./api/README.md", []byte("# `/api`"), 0755)
 	os.Mkdir("assets", 0755)
+	ioutil.WriteFile("./assets/README.md", []byte("# `/assets`"), 0755)
 	os.Mkdir("build", 0755)
+	ioutil.WriteFile("./build/README.md", []byte("# `/build`"), 0755)
 	os.Mkdir("cmd", 0755)
+	ioutil.WriteFile("./cmd/README.md", []byte("# `/cmd`"), 0755)
 	os.Mkdir("config", 0755)
+	ioutil.WriteFile("./config/README.md", []byte("# `/config`"), 0755)
 	os.Mkdir("deployments", 0755)
+	ioutil.WriteFile("./deployments/README.md", []byte("# `/deployments`"), 0755)
 	os.Mkdir("docs", 0755)
+	ioutil.WriteFile("./docs/README.md", []byte("# `/docs`"), 0755)
 	os.Mkdir("examples", 0755)
+	ioutil.WriteFile("./examples/README.md", []byte("# `/examples`"), 0755)
 	os.Mkdir("githooks", 0755)
+	ioutil.WriteFile("./githooks/README.md", []byte("# `/githooks`"), 0755)
 	os.Mkdir("init", 0755)
+	ioutil.WriteFile("./init/README.md", []byte("# `/init`"), 0755)
 	os.Mkdir("internal", 0755)
+	ioutil.WriteFile("./internal/README.md", []byte("# `/internal`"), 0755)
 	os.Mkdir("pkg", 0755)
+	ioutil.WriteFile("./pkg/README.md", []byte("# `/pkg`"), 0755)
 	os.Mkdir("scripts", 0755)
+	ioutil.WriteFile("./scripts/README.md", []byte("# `/scripts`"), 0755)
 	os.Mkdir("test", 0755)
+	ioutil.WriteFile("./test/README.md", []byte("# `/test`"), 0755)
 	os.Mkdir("third_party", 0755)
+	ioutil.WriteFile("./third_party/README.md", []byte("# `/third_party`"), 0755)
 	os.Mkdir("tools", 0755)
+	ioutil.WriteFile("./tools/README.md", []byte("# `/tools`"), 0755)
 	os.Mkdir("vendor", 0755)
+	ioutil.WriteFile("./vendor/README.md", []byte("# `/vendor`"), 0755)
 	os.Mkdir("web", 0755)
+	ioutil.WriteFile("./web/README.md", []byte("# `/web`"), 0755)
 	os.Mkdir("website", 0755)
-}
-
-func createFiles() {
+	ioutil.WriteFile("./website/README.md", []byte("# `/website`"), 0755)
 	gitignore := `# Binaries for programs and plugins
 *.exe
 *.exe~
 *.dll
 *.so
 *.dylib
-
 # Test binary, built with 'go test -c'
 *.test
-
 # Output of the go coverage tool, specifically when used with LiteIDE
 *.out
-
 # Dependency directories (remove the comment below to include it)
 # vendor/`
-
 	ioutil.WriteFile("./gitignore", []byte(gitignore), 0755)
-	ioutil.WriteFile("./api/README.md", []byte("# `/api`"), 0755)
-	ioutil.WriteFile("./assets/README.md", []byte("# `/assets`"), 0755)
-	ioutil.WriteFile("./build/README.md", []byte("# `/build`"), 0755)
-	ioutil.WriteFile("./cmd/README.md", []byte("# `/cmd`"), 0755)
-	ioutil.WriteFile("./config/README.md", []byte("# `/config`"), 0755)
-	ioutil.WriteFile("./deployments/README.md", []byte("# `/deployments`"), 0755)
-	ioutil.WriteFile("./docs/README.md", []byte("# `/docs`"), 0755)
-	ioutil.WriteFile("./examples/README.md", []byte("# `/examples`"), 0755)
-	ioutil.WriteFile("./githooks/README.md", []byte("# `/githooks`"), 0755)
-	ioutil.WriteFile("./init/README.md", []byte("# `/init`"), 0755)
-	ioutil.WriteFile("./internal/README.md", []byte("# `/internal`"), 0755)
-	ioutil.WriteFile("./pkg/README.md", []byte("# `/pkg`"), 0755)
-	ioutil.WriteFile("./scripts/README.md", []byte("# `/scripts`"), 0755)
-	ioutil.WriteFile("./test/README.md", []byte("# `/test`"), 0755)
-	ioutil.WriteFile("./third_party/README.md", []byte("# `/third_party`"), 0755)
-	ioutil.WriteFile("./tools/README.md", []byte("# `/tools`"), 0755)
-	ioutil.WriteFile("./vendor/README.md", []byte("# `/vendor`"), 0755)
-	ioutil.WriteFile("./web/README.md", []byte("# `/web`"), 0755)
-	ioutil.WriteFile("./website/README.md", []byte("# `/website`"), 0755)
 }
 
 func main() {
-	createDirectory()
-	createFiles()
+	createProject()
 }
