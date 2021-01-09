e561da0bbb06b2444bee48a75e9d4a2dbf321a70
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-01-09 02:39:11 -0500

    Update main.go

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"io/ioutil"
 	"os"
+	//"fmt"
 )
 
 func main() {
@@ -32,6 +33,10 @@ func createProject() {
 	ioutil.WriteFile("./build/package/README.md", []byte("# `/build/package`"), 0755)
 	os.Mkdir("cmd", 0755)
 	ioutil.WriteFile("./cmd/README.md", []byte("# `/cmd`"), 0755)
+	os.Chdir("cmd")
+	os.Mkdir(argument, 0755)
+	ioutil.WriteFile("./cmd/README.md", []byte("# `/cmd`"), 0755)
+	os.Chdir(argument)
 	os.Mkdir("config", 0755)
 	ioutil.WriteFile("./config/README.md", []byte("# `/config`"), 0755)
 	os.Mkdir("deployments", 0755)
@@ -73,7 +78,6 @@ func createProject() {
 func gomod() {
 	gomod := `module main
 	go 1.15`
-
 	ioutil.WriteFile("./go.mod", []byte(gomod), 0755)
 }
 
@@ -238,6 +242,5 @@ See the ['/website'](website/README.md) directory for examples.
 Some Go projects do have a 'src' folder, but it usually happens when the devs came from the Java world where it's a common pattern. If you can help yourself try not to adopt this Java pattern. You really don't want your Go code or Go projects to look like Java :-)
 
 Don't confuse the project level '/src' directory with the '/src' directory Go uses for its workspaces as described in ['How to Write Go Code'](https://golang.org/doc/code.html). The '$GOPATH' environment variable points to your (current) workspace (by default it points to '$HOME/go' on non-windows systems). This workspace includes the top level '/pkg', '/bin' and '/src' directories. Your actual project ends up being a sub-directory under '/src', so if you have the '/src' directory in your project the project path will look like this: '/some/path/to/workspace/src/your_project/src/your_code.go'. Note that with Go 1.11 it's possible to have your project outside of your 'GOPATH', but it still doesn't mean it's a good idea to use this layout pattern.`
-
 	ioutil.WriteFile("./README.md", []byte(readme), 0755)
 }
